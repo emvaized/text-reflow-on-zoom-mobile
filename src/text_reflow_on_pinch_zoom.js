5a17024a4b25bb9d6fec6d8f601fb2a1857735d5
Max <maximtsyba@gmail.com>
2024-12-03 20:29:50 +0100

    Don't scroll VIDEO elements into the view horizontally

--- src/text_reflow_on_pinch_zoom.js
+++ src/text_reflow_on_pinch_zoom.js
@@ -94,7 +94,7 @@
 
                 // Scroll element into view horizontally
                 // if (elementIsTextElement(zoomTarget)) {
-                if (zoomTarget.nodeName !== 'IMG'){
+                if (zoomTarget.nodeName !== 'IMG' && zoomTarget.nodeName !== 'VIDEO'){
                     zoomTarget.classList.add('text-reflow-scroll-padding')
                     zoomTarget.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
                     zoomTarget.classList.remove('text-reflow-scroll-padding')
